caf87de84861144a65de61d35124f795f4d679bb
joellustgarten <joel.lustgarten@gmail.com>
2023-09-30 19:38:18 -0500

    include php scripts

---
+++ PHP/controllerUserData.php
@@ -0,0 +1,28 @@
+<?php 
+session_start();
+require "connection.php";
+$email = "";
+$name = "";
+$errors = array();
+
+# if user click login button
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $check_email = "SELECT * FROM login WHERE user_email = '$email'";
+        $res = mysqli_query($conn, $check_email);
+        if(mysqli_num_rows($res) > 0){
+            $fetch = mysqli_fetch_assoc($res);
+            $fetch_pass = $fetch['password'];
+            if(password_verify($password, $fetch_pass)){
+                $_SESSION['email'] = $email;
+                header('location: menu.php');
+            }else{
+                $errors['email'] = "Incorrect email or password!";
+            }
+        }else{
+            $errors['email'] = "It's look like you're not yet a member! Click on the bottom link to signup.";
+        }
+    }
+
+?>    
